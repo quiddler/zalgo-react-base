6c9735507f0750777bb7a0fe968a096435660c2f
Eliot Easterling <eeasterling@fortecmedical.com>
2020-08-24 12:15:21 -0400

    wip

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders welcome message', () => {
+  const wrapper = shallow(<App />);
+  const welcome = <h2>Welcome to Zalgo</h2>;
+  expect(wrapper.contains(welcome)).toEqual(true);
 });
